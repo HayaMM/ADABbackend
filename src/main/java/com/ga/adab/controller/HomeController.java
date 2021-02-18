eb8b99fede1eefabf989f43be46a4b05b98d92a5
Ghaidaa Hussain <gadhus@outlook.com>
2021-02-18 13:42:29 +0300

    new branch

--- src/main/java/com/ga/adab/controller/HomeController.java
+++ src/main/java/com/ga/adab/controller/HomeController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
-
+//first controller
 	@GetMapping("/")
 	public String hello() {
 		return "Hello , this is our very first java wep appliction";
