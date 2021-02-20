851abfdffc8367bc201161ae64072a51da3ff557
Haya <hm.almadhi@gmail.com>
2021-02-20 21:40:29 +0300

    Quote edit method and test it with postmap

--- src/main/java/com/ga/adab/controller/QuoteController.java
+++ src/main/java/com/ga/adab/controller/QuoteController.java
@@ -3,6 +3,7 @@ package com.ga.adab.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,4 +30,10 @@ public Quote quoteDetails(@RequestParam int id) {
 Quote quote =dao.findById(id);
 return quote;
 }
+@PutMapping("/quote/edit")
+public Quote editQuote(@RequestBody Quote quote) {
+dao.save(quote);
+return quote;
+}
+
 }
