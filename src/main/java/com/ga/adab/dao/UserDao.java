00cd1a583eb92887bd103eb41496fa4a59eb87a0
Rasha Alkaabi <Rasha.a.1418@htmail.com>
2021-02-20 19:10:33 +0300

    User dao and model

---
+++ src/main/java/com/ga/adab/dao/UserDao.java
@@ -0,0 +1,12 @@
+package com.ga.adab.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.ga.adab.model.User;
+
+
+
+public interface UserDao extends CrudRepository<User, Integer> {
+		public User findByEmailAddress(String emailAddress);
+	
+}
