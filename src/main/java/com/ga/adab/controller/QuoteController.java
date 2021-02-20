19002349ed9999f3bc0e6db8ee2da842833fb2ce
Haya <hm.almadhi@gmail.com>
2021-02-20 21:06:49 +0300

    Quote add method and test it with postman

--- src/main/java/com/ga/adab/controller/QuoteController.java
+++ src/main/java/com/ga/adab/controller/QuoteController.java
@@ -1,5 +1,20 @@
 package com.ga.adab.controller;
 
-public class QuoteController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.ga.adab.dao.QuoteDao;
+import com.ga.adab.model.Quote;
+
+@RestController
+public class QuoteController {
+@Autowired
+private QuoteDao dao;
+@PostMapping("/quote/add")
+public Quote addQuote(@RequestBody Quote quote) {
+	dao.save(quote);
+	return quote;
+}
 }
