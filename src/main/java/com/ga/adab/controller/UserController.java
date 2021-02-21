167e34b626f9f7a8604c778ce3d8aa556914528b
Ghaidaa Hussain <gadhus@outlook.com>
2021-02-21 11:09:45 +0300

    user security

--- src/main/java/com/ga/adab/controller/UserController.java
+++ src/main/java/com/ga/adab/controller/UserController.java
@@ -5,13 +5,21 @@ import java.util.HashMap;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ga.adab.config.JwtUtil;
 import com.ga.adab.dao.UserDao;
+import com.ga.adab.model.JwtResponse;
 import com.ga.adab.model.User;
 
 
@@ -56,4 +64,32 @@ public class UserController {
 			dao.save(user);
 			return user;
 		}
+		
+		@Autowired
+		 AuthenticationManager authenticationManager;
+		 @Autowired
+		 JwtUtil jwtUtil;
+		 @Autowired
+		 UserDetailsService userDetailsService;
+		 
+		 @PostMapping("/user/authenticate")
+		 public ResponseEntity<?> authenticate(@RequestBody User user){
+			 
+			 try {
+				 authenticationManager.authenticate(
+						 new UsernamePasswordAuthenticationToken(user.getEmailAddress(),user.getPassword())
+						 );
+			 }
+			 catch(BadCredentialsException e) {
+				 String res="Incorrect username or password";
+				 return ResponseEntity.ok(res);
+			 }
+			 
+			 //Conitnue
+			 UserDetails userDetails=userDetailsService.loadUserByUsername(user.getEmailAddress());
+			 String jwtToken= jwtUtil.generateToken(userDetails);
+			 System.out.println(jwtToken);
+			return ResponseEntity.ok(new JwtResponse(jwtToken));
+			 
+		 }
 }
