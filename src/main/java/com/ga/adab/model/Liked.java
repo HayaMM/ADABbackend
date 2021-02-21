d7c6faaa570fd1b63a8b1be11f97f3b80a4b5957
Haya <hm.almadhi@gmail.com>
2021-02-21 10:50:11 +0300

    liked model comment realtionships

---
+++ src/main/java/com/ga/adab/model/Liked.java
@@ -0,0 +1,89 @@
+package com.ga.adab.model;
+
+import java.time.LocalDateTime;
+import javax.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity
+@Table(name="Liked")
+public class Liked {
+	@Id
+	@GeneratedValue
+	private int id;
+	
+	private boolean isQLiked;
+	
+	//// Relationship many likes belong to one user:
+//	@ManyToOne
+//	@JoinColumn(name="FK_UserId")
+//	private User user;
+	
+	//// Relationship many likes for one quote:
+//	@ManyToOne
+//	@JoinColumn(name="FK_QuoteId")
+//	private Quote quote;
+	
+	@Column(name="createdAt", nullable=false, updatable=false)
+	@CreationTimestamp
+	private LocalDateTime createAt;
+	
+	@Column(name="updatedat", nullable=false,updatable=true)
+	@UpdateTimestamp
+	private LocalDateTime updateAt;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	
+
+//	public User getUser() {
+//		return user;
+//	}
+//
+//	public void setUser(User user) {
+//		this.user = user;
+//	}
+
+	
+
+//	public Quote getQuote() {
+//		return quote;
+//	}
+//
+//	public void setQuote(Quote quote) {
+//		this.quote = quote;
+//	}
+
+	public LocalDateTime getCreateAt() {
+		return createAt;
+	}
+
+	
+	public boolean isQLiked() {
+		return isQLiked;
+	}
+
+	public void setQLiked(boolean isQLiked) {
+		this.isQLiked = isQLiked;
+	}
+
+	public void setCreateAt(LocalDateTime createAt) {
+		this.createAt = createAt;
+	}
+
+	public LocalDateTime getUpdateAt() {
+		return updateAt;
+	}
+
+	public void setUpdateAt(LocalDateTime updateAt) {
+		this.updateAt = updateAt;
+	}
+	
+
+}
