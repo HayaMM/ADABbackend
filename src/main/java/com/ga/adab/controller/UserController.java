1a0433caa62771efaacc1079421b4eb720fe9e1b
Ghaidaa Hussain <gadhus@outlook.com>
2021-02-21 16:23:20 +0300

    update user change password

--- src/main/java/com/ga/adab/controller/UserController.java
+++ src/main/java/com/ga/adab/controller/UserController.java
@@ -25,95 +25,92 @@ import com.ga.adab.dao.UserDao;
 import com.ga.adab.model.JwtResponse;
 import com.ga.adab.model.User;
 
-
-
 @RestController
 public class UserController {
 
-	
-	
-	
 	@Autowired
 	private UserDao dao;
-	
+
 	@Autowired
 	HttpServletRequest request;
-	
+
 	// To post the registration form
-	 @PostMapping("/user/registration")
-	 public HashMap<String, String> registration(@RequestBody User user) {
+	@PostMapping("/user/registration")
+	public HashMap<String, String> registration(@RequestBody User user) {
 
-		 HashMap<String, String> response = new HashMap<String, String> ();
-		 // Check to user is already registered or not
-		 var it = dao.findAll();
-		 
-		 for(User dbUser : it) {
-			 if(dbUser.getEmailAddress().equals(user.getEmailAddress())) {
-				 response.put("message", "User already exists");
-				 return response;
-			 }
-		 }
-		 
-		 // Password Encryption
-		 BCryptPasswordEncoder bCrypt = new BCryptPasswordEncoder();
-		 String newPassword = bCrypt.encode(user.getPassword());
-		 user.setPassword(newPassword);
-		 dao.save(user);
-		 response.put("message", "User registered successfully");
-		 return response;
-		 
-	 }
-	 
-	// HTTP Put REQUEST - User Edit
-		@PutMapping("/user/edit")
-		public User editUser(@RequestBody User user) {
-			dao.save(user);
-			return user;
-		}
-		
-		@Autowired
-		 AuthenticationManager authenticationManager;
-		 @Autowired
-		 JwtUtil jwtUtil;
-		 @Autowired
-		 UserDetailsService userDetailsService;
-		 
-		 @PostMapping("/user/authenticate")
-		 public ResponseEntity<?> authenticate(@RequestBody User user){
-			 
-			 try {
-				 authenticationManager.authenticate(
-						 new UsernamePasswordAuthenticationToken(user.getEmailAddress(),user.getPassword())
-						 );
-			 }
-			 catch(BadCredentialsException e) {
-				 String res="Incorrect username or password";
-				 return ResponseEntity.ok(res);
-			 }
-			 
-			 //Conitnue
-			 UserDetails userDetails=userDetailsService.loadUserByUsername(user.getEmailAddress());
-			 String jwtToken= jwtUtil.generateToken(userDetails);
-			 System.out.println(jwtToken);
-			return ResponseEntity.ok(new JwtResponse(jwtToken));
-			 
-		 }
-		 
-		// HTTP GET REQUEST - User Detail
-			@PostMapping("/user/changepassword")
-			public User userDetails(@RequestParam int id,@RequestBody ObjectNode json) {
-				String Password = json.get("password").asText(); 
-				String newPassword = json.get("newPassword").asText(); 
-				System.out.println(Password);
-				System.out.println(newPassword);
+		HashMap<String, String> response = new HashMap<String, String>();
+		// Check to user is already registered or not
+		var it = dao.findAll();
 
-				User user = dao.findById(id);
-				BCryptPasswordEncoder bCrypt = new BCryptPasswordEncoder();
-				 String cryptPassword = bCrypt.encode(newPassword);
-				 user.setPassword(cryptPassword);
-				 dao.save(user);
-				System.out.println(user.getPassword());
-
-				return user;
+		for (User dbUser : it) {
+			if (dbUser.getEmailAddress().equals(user.getEmailAddress())) {
+				response.put("message", "User already exists");
+				return response;
 			}
+		}
+
+		// Password Encryption
+		BCryptPasswordEncoder bCrypt = new BCryptPasswordEncoder();
+		String newPassword = bCrypt.encode(user.getPassword());
+		user.setPassword(newPassword);
+		dao.save(user);
+		response.put("message", "User registered successfully");
+		return response;
+
+	}
+
+	// HTTP Put REQUEST - User Edit
+	@PutMapping("/user/edit")
+	public User editUser(@RequestBody User user) {
+		dao.save(user);
+		return user;
+	}
+
+	@Autowired
+	AuthenticationManager authenticationManager;
+	@Autowired
+	JwtUtil jwtUtil;
+	@Autowired
+	UserDetailsService userDetailsService;
+
+	@PostMapping("/user/authenticate")
+	public ResponseEntity<?> authenticate(@RequestBody User user) {
+
+		try {
+			authenticationManager
+					.authenticate(new UsernamePasswordAuthenticationToken(user.getEmailAddress(), user.getPassword()));
+		} catch (BadCredentialsException e) {
+			String res = "Incorrect username or password";
+			return ResponseEntity.ok(res);
+		}
+
+		// Conitnue
+		UserDetails userDetails = userDetailsService.loadUserByUsername(user.getEmailAddress());
+		String jwtToken = jwtUtil.generateToken(userDetails);
+		System.out.println(jwtToken);
+		return ResponseEntity.ok(new JwtResponse(jwtToken));
+
+	}
+
+	// HTTP GET REQUEST - User Detail
+	@PostMapping("/user/changepassword")
+	public User userDetails(@RequestParam int id, @RequestBody ObjectNode json) {
+
+		BCryptPasswordEncoder bCrypt = new BCryptPasswordEncoder();
+		User user = dao.findById(id);
+		String Password = json.get("password").asText();
+		String newPassword = json.get("newPassword").asText();
+
+		if (bCrypt.matches(Password, user.getPassword())) {
+			String cryptPassword = bCrypt.encode(newPassword);
+			user.setPassword(cryptPassword);
+			dao.save(user);
+			System.out.println("equless");
+
+		} else {
+			System.out.println("not equless");
+		}
+
+		return user;
+	}
 }
