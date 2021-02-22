e05152d5c96bacc64d964da36795487420e91438
Ghaidaa Hussain <gadhus@outlook.com>
2021-02-22 13:48:00 +0300

    adding like counting query

--- src/main/java/com/ga/adab/controller/LikedController.java
+++ src/main/java/com/ga/adab/controller/LikedController.java
@@ -1,7 +1,8 @@
 package com.ga.adab.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,7 +13,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ga.adab.dao.LikedDao;
+import com.ga.adab.dao.QuoteDao;
+import com.ga.adab.dao.UserDao;
 import com.ga.adab.model.Liked;
+import com.ga.adab.model.Quote;
+import com.ga.adab.model.User;
 
 @RestController
 public class LikedController {
@@ -20,9 +25,23 @@ public class LikedController {
 	@Autowired
 	private LikedDao dao;
 	
+	@Autowired
+	private QuoteDao quotedao;
+	
+	@Autowired
+	private UserDao userdao;
+	
+	@Transactional
 	@PostMapping("/liked/add")
-	public Liked addLiked(@RequestBody Liked liked) {
+	public Liked addLiked(@RequestBody Liked liked ) {
+		Quote q = quotedao.findById(25);
+		int id = q.getId();
+		User u = userdao.findById(10);
+		liked.setQuote(q);
+		liked.setUser(u);
 		dao.save(liked);
+		int likes = dao.getlikes(id);
+		quotedao.setlikes(id, likes);
 		return liked;
 	}
 	@GetMapping("/liked/index")
--- src/main/java/com/ga/adab/dao/QuoteDao.java
+++ src/main/java/com/ga/adab/dao/QuoteDao.java
@@ -1,9 +1,18 @@
 package com.ga.adab.dao;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
 import com.ga.adab.model.Quote;
 
 public interface QuoteDao extends CrudRepository<Quote, Integer>{
 public Quote findById(int id);
 
+
+
+@Modifying
+@Query( value="UPDATE adab.quote SET qreivew =:likes WHERE id =:id", nativeQuery  = true)
+public	int setlikes(@Param("id") int id, int likes);
 }
